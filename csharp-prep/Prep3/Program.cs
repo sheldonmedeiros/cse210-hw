ef38506eac85dd63debbb1a8df2657c4566019dd
sheldonmedeiros <shizzmede13@gmail.com>
2023-04-26 20:43:38 -0600

    hw is done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random Random = new Random();
+        bool playAgain = true;
+        int min = 1;
+        int max = 100;
+        int guess;
+        int number;
+        int guesses;
+        String response;
+
+        while (playAgain == true)
+        {
+            guess = 0;
+            guesses = 0;
+            response = "";
+            number = Random.Next(min, max + 1 );
+        
+            while (guess != number)
+            {
+                Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Guess: " + guess );
+
+                if (guess > number)
+                {
+                    Console.WriteLine(guess + "is to high!");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine( guess + "is to low!");
+                }
+                guesses ++;
+            }
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("YOU WIN!");
+            System.Console.WriteLine("Guesses: " + guesses);
+
+            System.Console.WriteLine("Would you like to play again? (Y/N)");
+            response = Console.ReadLine();
+            response = response.ToUpper();
+
+            if (response == "Y")
+            {
+                playAgain = true;
+            }
+            else
+            {
+                playAgain = false;
+            }
+        }
+
+        Console.WriteLine("Thanks for Playing!");
+        Console.ReadKey();
     }
 }
